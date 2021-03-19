81101c1d8caecc315eb683f9f4662235507e2900
Ludovic RAVINET <ludovic.ravinet@gmail.com>
2021-03-19 15:09:23 +0100

    add scale button & link button

--- methodes.php
+++ methodes.php
@@ -1,94 +1,93 @@
+<?php
 
-    <?php
+include 'INC/header.php';
 
-    include 'INC/header.php';
-    
-    ?>
-    
-    <div class = "methode-title">
-        <h1>Prestations</h1>
-    </div>
-    
-    <div class = "methode-main-box">
-        <div class="methode-parag">
-            <div class = "methode-parag-text">
-                <div class = "methode-parag-text-title">
-                    <h2>Régime de printemps</h2>
-                    <p class = "methode-button-plus">En savoir plus !</p>              
-                </div>
-                <p class = "parag-text-par">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet 
-                    ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus 
-                    ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
-                    arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare 
-                    eget non nisi. Aliquam
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet 
-                    ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus 
-                    ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
-                    arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare 
-                    eget non nisi. Aliquam
-                </p> 
+?>
+
+<div class="methode-title">
+    <h1>Prestations</h1>
+</div>
+
+<div class="methode-main-box">
+    <div class="methode-parag">
+        <div class="methode-parag-text">
+            <div class="methode-parag-text-title">
+                <h2>Régime de printemps</h2>
+                <p class="methode-button-plus"><a href="contact.php">En savoir plus !</a></p>
             </div>
-            
-            <div id = "img1" class = "methode-image"></div>
-        </div>     
-
-        <div class="methode-parag">
-            <div id = "img2" class = "methode-image"></div>
-
-            <div class = "methode-parag-text">
-                <div class = "methode-parag-text-title">
-                    <h2>Lutter contre l'obésité</h2>
-                    <p class = "methode-button-plus">En savoir plus !</p>
-                </div>
-                <p class = "parag-text-par">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet 
-                    ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus 
-                    ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
-                    arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare 
-                    eget non nisi. Aliquam
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet 
-                    ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus 
-                    ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
-                    arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare 
-                    eget non nisi. Aliquam
-                </p>
-            </div>
+            <p class="parag-text-par">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet
+                ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus
+                ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
+                arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare
+                eget non nisi. Aliquam
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet
+                ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus
+                ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
+                arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare
+                eget non nisi. Aliquam
+            </p>
         </div>
 
-        <div class="methode-parag">
-            <div class = "methode-parag-text">
-                <div class = "methode-parag-text-title">
-                    <h2>Régime thérapeutique</h2>
-                    <p class = "methode-button-plus">En savoir plus !</p>
-                </div>
-                <p class = "parag-text-par">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet 
-                    ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus 
-                    ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
-                    arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare 
-                    eget non nisi. Aliquam
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet 
-                    ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus 
-                    ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
-                    arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare 
-                    eget non nisi. Aliquam
-                </p>
+        <div id="img1" class="methode-image"></div>
+    </div>
+
+    <div class="methode-parag">
+        <div id="img2" class="methode-image"></div>
+
+        <div class="methode-parag-text">
+            <div class="methode-parag-text-title">
+                <h2>Lutter contre l'obésité</h2>
+                <p class="methode-button-plus"><a href="contact.php">En savoir plus !</a></p>
             </div>
-            
-            <div id = "img3" class = "methode-image"></div>
+            <p class="parag-text-par">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet
+                ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus
+                ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
+                arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare
+                eget non nisi. Aliquam
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet
+                ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus
+                ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
+                arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare
+                eget non nisi. Aliquam
+            </p>
+        </div>
+    </div>
+
+    <div class="methode-parag">
+        <div class="methode-parag-text">
+            <div class="methode-parag-text-title">
+                <h2>Régime thérapeutique</h2>
+                <p class="methode-button-plus"><a href="contact.php">En savoir plus !</a></p>
+            </div>
+            <p class="parag-text-par">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet
+                ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus
+                ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
+                arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare
+                eget non nisi. Aliquam
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet
+                ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus
+                ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
+                arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare
+                eget non nisi. Aliquam
+            </p>
         </div>
 
-        <p class = "next-sentence">Le bien-être, à quel prix !</p>
-        <p class = "next-button">Mes tarifs</p> 
-
+        <div id="img3" class="methode-image"></div>
     </div>
-    
-    <?php
 
-    include 'INC/footer.php';
+    <p class="next-sentence">Le bien-être, à quel prix !</p>
+    <p class="next-button"><a href="tarifs.php">Mes tarifs</a></p>
 
-    ?>
+</div>
+
+<?php
+
+include 'INC/footer.php';
+
+?>
--- tarifs.php
+++ tarifs.php
@@ -1,68 +1,67 @@
+<?php
 
-    <?php
+include 'INC/header.php';
 
-    include 'INC/header.php';
+?>
 
-    ?>
+<div class="tarif-title">
+    <h1>Tarifs</h1>
+</div>
 
-    <div class = "tarif-title">
-        <h1>Tarifs</h1>
+<div class="tarif-main-box">
+    <h2>Mes abonnements</h2>
+    <div class="tarif-all-subscription">
+        <div class="tarif-subscription">
+            <p class="tarif-month">3 mois</p>
+            <p>150 € / mois</p>
+            <p>2 séances / mois</p>
+        </div>
+        <div class="tarif-subscription">
+            <p class="tarif-month">6 mois</p>
+            <p>120 € / mois</p>
+            <p>2 séances / mois</p>
+        </div>
+        <div class="tarif-subscription">
+            <p class="tarif-month">12 mois</p>
+            <p>100 € / mois</p>
+            <p>2 séances / mois</p>
+        </div>
     </div>
 
-    <div class = "tarif-main-box">
-        <h2>Mes abonnements</h2>
-        <div class = "tarif-all-subscription">
-            <div class = "tarif-subscription">
-                <p class = "tarif-month">3 mois</p>
-                <p>150 € / mois</p>
-                <p>2 séances / mois</p>
-            </div>
-            <div class = "tarif-subscription">
-                <p class = "tarif-month">6 mois</p>
-                <p>120 € / mois</p>
-                <p>2 séances / mois</p>
-            </div>
-            <div class = "tarif-subscription">
-                <p class = "tarif-month">12 mois</p>
-                <p>100 € / mois</p>
-                <p>2 séances / mois</p>
-            </div>
-        </div>
-            
 
-        <p>Chaque séance dure 30 mn. La formule Abonnement inclut
-            un bilan préliminaire et un suivi personnalisé. Les 
-            conseils téléphoniques font partie intégrante de la 
-            prestation. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet 
-            ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus 
+    <p>Chaque séance dure 30 mn. La formule Abonnement inclut
+        un bilan préliminaire et un suivi personnalisé. Les
+        conseils téléphoniques font partie intégrante de la
+        prestation. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet
+        ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus
+        ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
+        arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare
+        eget non nisi. Aliquam
+    </p>
+
+    <div class="tarif-no-subscription">
+        <h2>Hors abonnement</h2>
+        <p>Je propose aussi des séances individuelles de coaching régime à 75 € la séance.
+            Un bilan complet de vos habitudes alimentaire est réalisable au prix de 100 €.
+            Je propose aussi des séances individuelles de coaching régime à 75 € la séance.
+            Un bilan complet de vos habitudes alimentaire est réalisable au prix de 100 €.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet
+            ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus
             ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
             consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
-            arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare 
+            arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare
             eget non nisi. Aliquam
         </p>
+    </div>
 
-        <div class = "tarif-no-subscription">
-            <h2>Hors abonnement</h2>
-            <p>Je propose aussi des séances individuelles de coaching régime à 75 € la séance.
-                Un bilan complet de vos habitudes alimentaire est réalisable au prix de 100 €. 
-                Je propose aussi des séances individuelles de coaching régime à 75 € la séance.
-                Un bilan complet de vos habitudes alimentaire est réalisable au prix de 100 €.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc urna dui, aliquet 
-                ut libero ac, fermentum feugiat arcu. Ut dignissim blandit efficitur. Phasellus 
-                ut metus at orci imperdiet ornare eget non nisi. Aliquam Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit. Nunc urna dui, aliquet ut libero ac, fermentum feugiat
-                arcu. Ut dignissim blandit efficitur. Phasellus ut metus at orci imperdiet ornare 
-                eget non nisi. Aliquam
-            </p>
-        </div>
-        
-        <p class = "next-sentence">Prêt à prendre soin de vous ?</p>
-        <p class = "next-button">Contactez moi</p> 
+    <p class="next-sentence">Prêt à prendre soin de vous ?</p>
+    <p class="next-button"><a href="contact.php">Contactez moi</p>
 
-    </div> 
+</div>
 
-    <?php
+<?php
 
-    include 'INC/footer.php';
+include 'INC/footer.php';
 
-    ?>
+?>
