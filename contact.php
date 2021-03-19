83fc62ef2cfe2268c01ef418e74bab1c655e9aa9
furkanKrdmr <karademirfurkan69@gmail.com>
2021-03-19 16:45:07 +0100

    edited style.css,contact.php,presentation.php

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 
     <?php
 
-include 'INC/header.php';
+    include 'INC/header.php';
 
     ?>
 
@@ -13,27 +13,21 @@ include 'INC/header.php';
 
         <div class="txt1">
             <div class="bloctext">
-                <p style="text-align: center; font-size: 18pt;"><strong>Maryse Thiebaud</strong></p>
+                <p style="text-align: center;"><strong>Maryse Thiebaud</strong></p>
                 <p style="text-align: center;">4 avenue des plantées</p>
                 <p style="text-align: center;">69330 Meyzieu <br> Tél: 06 24 24 24 24</p>
                 <p style="text-align: center;"><strong>Tram T3 : Gare de Part Dieu la Villete</strong></p>
-            </div>
-            <div class="imgrdv">
                 <img class="imgrdvv" src="img/Horaires.png" alt="">
             </div>
 
-            <div class="img">
-                <img class="femme" src="img/femme.jpg" alt="" style="height: 25em; width: 88%;">
-            </div>
 
             <div class="carteggle">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1655.0974408355808!2d4.859503140963323!3d45.76054737910553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ea62fdaf6429%3A0x6e097152ab2851f8!2sLyon%20Part%20Dieu!5e0!3m2!1sfr!2sfr!4v1615993200539!5m2!1sfr!2sfr"
                     allowfullscreen="" loading="lazy"></iframe>
             </div>
-        </div>
 
-        <div class="container">
+            <div class="container">
             <h1>Formulaire de contact</h1>
             <form action="/action_page.php">
                 <label for="fname">Nom & prénom</label>
@@ -50,6 +44,7 @@ include 'INC/header.php';
 
                 <input type="submit" value="Envoyer">
             </form>
+        </div>
         </div>
     </main>
 
